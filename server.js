a549ae1da5f59b876d7d3f5304f1b787522891ec
vuhuutrieu <vuhuutrieu@gmail.com>
2020-10-06 13:23:52 +0700

    Initial commit21

--- server.js
+++ server.js
@@ -2,7 +2,12 @@ var express = require('express'),
 
   app = express(),
   port = process.env.PORT || 3000;
-
+  app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 app.listen(port);
 var routes = require('./api/routes/todoRoutes'); //importing route
 routes(app); //register the route
