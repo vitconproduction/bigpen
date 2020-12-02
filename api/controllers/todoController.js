1d9528fb71a36da814ea6f66ba3bed9753f2079b
vuhuutrieu <vuhuutrieu@gmail.com>
2020-12-02 21:10:53 +0700

    Initial commit

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -5,8 +5,6 @@
 
 exports.list_all_tasks = function(req, res) {
     const readability = require('readability-nodejs')
- 
-  
 
     let url=decodeURIComponent(req.params.url)
     var request = require('sync-request');
@@ -25,7 +23,27 @@ let article = reader.parse();
 };
 
 
+exports.list_nlp = function(req, res) {
+    const readability = require('readability-nodejs')
 
+    let url=decodeURIComponent(req.params.url)
+    var request = require('sync-request');
+var resp = request('GET', url, {
+  headers: {
+    'user-agent': 'example-user-agent',
+  },
+});
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM(resp.getBody('utf-8'));
+
+let reader = new readability.Readability(dom.window.document);
+let article = reader.parse();
+var vntk = require('vntk');
+var tokenizer = vntk.wordTokenizer();
+let wordarray=tokenizer.tag(article.textContent)
+    res.json({"data":wordarray});
+};
 
 exports.create_a_task = function(req, res) {
     res.json({"trieu":"vu"});
