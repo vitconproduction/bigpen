a64ab299943f24b11ac51412b77b1907a88c7dc1
vuhuutrieu <vuhuutrieu@gmail.com>
2020-12-24 08:50:00 +0700

    Initial commit

--- api/routes/todoRoutes.js
+++ api/routes/todoRoutes.js
@@ -1,15 +1,18 @@
 'use strict';
 module.exports = function(app) {
+  const express = require('express');
   var todoList = require('../controllers/todoController');
-
+  const path = require('path');
+  const router = express.Router();
   // todoList Routes
   app.route('/tasks/:url')
     .get(todoList.list_all_tasks)
     .post(todoList.create_a_task);
  app.route('/nlp/:url')
     .get(todoList.list_nlp)
-  
-
+    app.get('/bigpen',function(req,res){
+      res.sendFile(path.join(__dirname+'/bigpen.html'));
+    });
   app.route('/tasks/:taskId')
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
