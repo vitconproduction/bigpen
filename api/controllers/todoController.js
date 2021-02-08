55cdcc2576d135072b92a384115c776c936d11d5
vuhuutrieu <vuhuutrieu@gmail.com>
2021-02-08 10:05:33 +0700

    Initial commit

--- api/controllers/todoController.js
+++ api/controllers/todoController.js
@@ -28,6 +28,8 @@ var resp = request('GET', url, {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36',
   },
 });
+res.send(resp.getBody('utf-8'))
+/*
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const dom = new JSDOM(resp.getBody('utf-8'));
@@ -35,6 +37,7 @@ const dom = new JSDOM(resp.getBody('utf-8'));
 let reader = new readability.Readability(dom.window.document);
 let article = reader.parse();
     res.json({"data":article});
+    */
 };
 
 exports.list_nlp = function(req, res) {
